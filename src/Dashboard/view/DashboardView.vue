e2efcc7921474b0b77b3ec910a521a5c0e2d36d3
aryant-nigam <aryantnigam@gmail.com>
2024-10-07 12:04:53 +0530

    refactored code

--- src/Dashboard/view/DashboardView.vue
+++ src/Dashboard/view/DashboardView.vue
@@ -1,45 +1,13 @@
 <template>
-    <div class="container-md">
-		<div class="row d-flex justify-content-center px-2">
-            <h3 class="text-center py-4">Dashboard</h3>
-            <NavigativeElements 
-                :tabs="tabData" 
-                :initial-tab="initialTab"
-            />
-		</div>
-	</div>
+    <DashboardNavigation/>
 </template>
 
 <script>
-import NavigativeElements from '@/common/components/NavigativeElements.vue';
-import DashboardPage from '@/Dashboard/components/DashboardPage.vue';
-import ChatsPage from '@/Dashboard/components/ChatsPage.vue';
+import DashboardNavigation from '../components/DashboardNavigation.vue';
 export default{
     name:"DashboardView",
     components:{    
-        NavigativeElements,
-    },
-    computed: {
-		initialTab() {
-			return this.$route.query.tab || "Dashboard";
-		}
-	},
-    data(){
-        return {
-            tabData: [
-				{
-					name: 'Dashboard',
-					title: 'Dashboard',
-                    iconName: ['fas', 'table-cells-large'],
-					component: DashboardPage
-				},
-				{
-					name: 'Chats',
-					title: 'Chats',
-                    iconName: ['far', 'comment'],
-					component: ChatsPage
-				}
-			]}
+        DashboardNavigation,
     }
 }
 </script>
